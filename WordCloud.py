c19f350aba705bd21494f63926597adb4a84deee
Jakub <kontakt@jakub0301.net>
2019-07-04 20:34:42 +0200

    Added WordCloud script

---
+++ WordCloud.py
@@ -0,0 +1,10 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+text = "C++ Java JavaScript Go Swift Assembly Mathematica Bash C# Pascal C Python HTML CSS PHP SQL Kotlin MATLAB "
+
+cloud = WordCloud(background_color = "white" ).generate(text)
+
+plt.imshow(cloud)
+plt.axis("off")
+plt.show()
